9860893c13b771b229da56a6a6388dd22bbd8beb
Locotine <nam192168@gmail.com>
2024-03-18 12:20:53 +0700

    fix: disable cache

--- src/app/modules/client/home/home.component.ts
+++ src/app/modules/client/home/home.component.ts
@@ -1,15 +1,46 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 import {NbCardModule} from '@nebular/theme';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NbCardModule
+    NbCardModule,
+    NgForOf
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  featured = [
+    {
+      img: './assets/image/product/basic-cat.jpg',
+      title: 'Basic cat plush',
+      category: 'Animal-plush',
+      price: 90000,
+      description: 'A basic, white, round cat'
+    },
+    {
+      img: './assets/image/product/chicken-leg.jpg',
+      title: 'Chicken leg plush',
+      category: 'Food-plush',
+      price: 90000,
+      description: 'A big chicken leg plush'
+    },
+    {
+      img: './assets/image/product/sans.jpg',
+      title: 'Sans plush',
+      category: 'Game-plush',
+      price: 80000,
+      description: 'Bad time'
+    },
+    {
+      img: './assets/image/product/ninja-cat.jpg',
+      title: 'Ninja cat plush',
+      category: 'Animal-plush',
+      price: 90000,
+      description: 'A basic, white, round cat wearing a ninja costume'
+    }
+  ];
 }
